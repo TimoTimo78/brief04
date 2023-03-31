ef3ebc7215f571e374c8231fbdf9a4ba8e1deeb9
Cedric <116574782+TimoTimo78@users.noreply.github.com>
2023-03-31 15:51:20 +0200

    Adapt PopUp

--- app.js
+++ app.js
@@ -13,128 +13,53 @@ document.body.onload=function(){
         div.style.backgroundImage="url('img/im"+i+".jpg')";
         container.appendChild(div);
     }
-    afficherMasquer();
 }
 d.onclick=function(){
     if(p<0)
     p++;
     container.style.transform="translate("+p*400+"px)";
     container.style.transition="all 0.5s ease";
-    afficherMasquer();
 }
 g.onclick=function(){
     if(p>-nbr+1)
     p--;
     container.style.transform="translate("+p*400+"px)";
     container.style.transition="all 0.5s ease";
-    afficherMasquer();
 }
+
+
 // ************************************popUp************************************
 
 
 
+const popup = document.createElement('div');
+    popup.classList.add('popup');
 
-// HTML
-/* <button id="myButton">Cliquez ici</button> */
-
-
-// JavaScript
-
-
-// const container = document.getElementById("container");
-// const card = document.getElementsByClassName("card");
-// const popUp = document.createElementByClassName("card");
-// popup.innerHTML = "Ceci est une description";
-// popup.style.display = "none";
-// popup.style.position = "absolute";
-// popup.style.top = "50px";
-// popup.style.left = "50px";
-// popup.style.padding = "10px";
-// popup.style.backgroundColor = "#fff";
-// popup.style.border = "1px solid #000";
-// popup.style.zIndex = "999";
-// document.body.appendChild(popup);
-// myButton.addEventListener("mouseover", () => {
-//   popup.style.display = "block";
-// });
-// myButton.addEventListener("mouseout", () => {
-//   popup.style.display = "none";
-// });
+const boutons = document.querySelectorAll('container button')
 
 
 
-// Dans cet exemple, nous avons créé un bouton avec l'ID myButton.
-//  Nous avons également créé une div qui contiendra notre
-//   popup de description. Nous avons ajouté du contenu à la div en
-//    utilisant la propriété innerHTML. Nous avons ensuite défini les
-//    styles CSS pour la div en utilisant les propriétés de style
-//     JavaScript.
+for (let index = 0; index < boutons.length; index++) {
+  const card = boutons[index];
+  console.log(jeu.getAttribute('id'));
+  popUp.addEventListener('click', ()=>{
+      popUp.classList.toggle('.card button');
+      });
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let  = document.querySelector(".profile");
-// let cardContent = document.querySelector(".cardContent");
-// profile.addEventListener("click", function() {
-//     cardContent.classList.toggle("active")
-// })
-// cardContent.addEventListener("click", function() {
-//     cardContent.classList.toggle("active")
-// })
-
-
-
-
-
-
-
-
-
-
-// let popUp = document.querySelectorAll('[cardContent]');
-
-// popupsBtn.forEach((btn) => {
-//     btn.addEventListener("click", activePopup);
-  
-//     function activePopup() {
-//       let popupId = btn.getAttribute("data-popup-ref");
-//       let popup = document.querySelector(`[data-popup-id='${popupId}']`);
-  
-//       if (popup !== undefined && popup !== null) {
-//         let popupContent = popup.querySelector(".popup-content");
-//         let closeBtns = popup.querySelectorAll("[data-dismiss-popup]");
-  
-//         closeBtns.forEach((btn) => {
-//           btn.addEventListener("click", onPopupClose);
-//         });
-//         popup.addEventListener("click", onPopupClose);
-  
-//         popupContent.addEventListener("click", (ev) => {
-//           ev.stopPropagation();
-//         });
-  
-//         popup.classList.add("active");
-//         setTimeout(() => {
-//           popupContent.classList.add("active");
-//         }, 1);
-  
-//         function onPopupClose() {
-//           setTimeout(() => {
-//             popup.classList.remove("active");
-//           }, 250);
-//           popupContent.classList.remove("active");
-//         }
-//       }
-//     }
-//   });
+let popUp = document.querySelectorAll('.card button');
+popUp.forEach((element) => {
+    element.addEventListener("click",() => {
+      let div = document.createElement('div');
+      let img = document.createElement('img');
+      let h2 = document.createElement('h2');
+      let p = document.createElement('p');
+      let body = document.querySelector('body');
+      div.append(img);
+      div.append(h2);
+      div.append(p);
+      console.log(div);
+      document.body.appendChild(div);
+    })
+  });
