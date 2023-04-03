de99bffdc8dfd68643dcb82a7d6aae84a235bbec
Cedric <116574782+TimoTimo78@users.noreply.github.com>
2023-04-03 10:57:33 +0200

    PopUp

--- app.js
+++ app.js
@@ -26,41 +26,7 @@ g.onclick=function(){
     container.style.transform="translate("+p*400+"px)";
     container.style.transition="all 0.5s ease";
 }
-
 // ************************************popUp************************************
-
-// Je vais continuer à adpater ma popUp à la page Web
-
-// const popup = document.createElement('div');
-//     popup.classList.add('popup');
-
-// const boutons = document.querySelectorAll('container button')
-
-// for (let index = 0; index < boutons.length; index++) {
-//   const card = boutons[index];
-//   console.log(jeu.getAttribute('id'));
-//   popUp.addEventListener('click', ()=>{
-//       popUp.classList.toggle('.card button');
-//       });
-// }
-
-// let popUp = document.querySelectorAll('.card button');
-// popUp.forEach((element) => {
-//     element.addEventListener("click",() => {
-//       let div = document.createElement('div');
-//       let img = document.createElement('img');
-//       let h2 = document.createElement('h2');
-//       let p = document.createElement('p');
-//       let body = document.querySelector('body');
-//       div.append(img);
-//       div.append(h2);
-//       div.append(p);
-//       console.log(div);
-//       document.body.appendChild(div);
-//     })
-//   });
-
-  // Sélectionner tous les boutons "Get more info"
 
 const buttons = document.querySelectorAll('.card button');
 const imgTab =document.querySelectorAll('.card img');
@@ -70,29 +36,65 @@ for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
     
     // Créer la popup
-
+    
     const popup = document.createElement('div');
     popup.classList.add('popup');
     popup.innerHTML = `
-      <div class="popupContent">
-        <img src='${imgTab[i].src}'>
-        <h2>${this.previousElementSibling.textContent}</h2>
-        <p>${this.previousElementSibling.querySelector('.cardContent p').textContent}</p>
-        <button class="closeBtn">Close</button>
-      </div> `;
+    <div class="popupContent">
+    <img src='${imgTab[i].src}'>
+    <h2>${this.previousElementSibling.textContent}</h2>
+    <p>${this.previousElementSibling.querySelector('.cardContent p').textContent}</p>
+    <button class="closeBtn">Close</button>
+    </div> `;
     const sections = document.body.querySelectorAll('section');
     let thirdSection = sections[2];
     thirdSection.appendChild(popup);
-
+    
     // document.body.appendChild(popup);
-
+    
     console.log(buttons);
-
+    
     // Ajouter un événement de clic au bouton "Close"
-
+    
     const closeBtn = popup.querySelector('.closeBtn');
     closeBtn.addEventListener('click', function() {
       popup.remove();
     });
   });
+
+
+
+
+  // Je vais continuer à adpater ma popUp à la page Web
+  
+  // const popup = document.createElement('div');
+  //     popup.classList.add('popup');
+  
+  // const boutons = document.querySelectorAll('container button')
+  
+  // for (let index = 0; index < boutons.length; index++) {
+  //   const card = boutons[index];
+  //   console.log(jeu.getAttribute('id'));
+  //   popUp.addEventListener('click', ()=>{
+  //       popUp.classList.toggle('.card button');
+  //       });
+  // }
+  
+  // let popUp = document.querySelectorAll('.card button');
+  // popUp.forEach((element) => {
+  //     element.addEventListener("click",() => {
+  //       let div = document.createElement('div');
+  //       let img = document.createElement('img');
+  //       let h2 = document.createElement('h2');
+  //       let p = document.createElement('p');
+  //       let body = document.querySelector('body');
+  //       div.append(img);
+  //       div.append(h2);
+  //       div.append(p);
+  //       console.log(div);
+  //       document.body.appendChild(div);
+  //     })
+  //   });
+  
+    // Sélectionner tous les boutons "Get more info"
 }
